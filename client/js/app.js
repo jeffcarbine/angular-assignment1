e86266a53631d4276c875a67950bfeec1954b512
jeffcarbine <jeffcarbine@gmail.com>
2015-09-30 15:18:27 -0600

    Trying again
    It gave me a conflict error last time. Let’s try again.

--- client/js/app.js
+++ client/js/app.js
@@ -1,66 +1,75 @@
-angular
-	.module('RepsApp', [
-		'RepsAppControllers'
-	]);
+
+angular.module('RepsApp', [
+  'RepsAppControllers'
+]);
 
 angular
-	.module('RepsAppControllers', [
-		'repsService'
-	])
-	.controller('MainCtrl', function (reps) {
-		var main = this;
-		main.reps = [];
-		main.congressType = 'reps';
+  .module('RepsAppControllers', [
+    'repsService'
+  ])
+  .controller('MainCtrl', function (reps) {
+    var main = this;
+    main.reps = [];
+    main.congressType = 'reps'; // or sens
 
-	main.searchAllByZip = function(zip) {
-		reps.allByZip(zip).then(function (data) {
-			main.reps = data;
-		});
-	};
+    main.loading = false;
 
-	main.searchRepsByName = function(zip) {
-		reps.repsByName(name).then(function (data) {
-			main.reps = data;
-		});
-	};
+    main.loading = false;
 
-	main.searchRepsByState = function(zip) {
-		reps.repsByState(state).then(function (data) {
-			main.reps = data;
-		});
-	};
+    main.apis = [
+      {
+        label: 'Zip',
+        method: function (zip) {
+          main.loading = true;
+          reps('all', 'zip', zip).then(function (data) {
+            main.reps = data;
+            main.loading = false;
+          });
+        }
+      },
+      {
+        label: 'Last Name',
+        method: function (name) {
+          main.loading = true;
+          reps(main.congressType, 'name', name).then(function (data) {
+            main.reps = data;
+            main.loading = false;
+          });
+        }
+      },
+      {
+        label: 'State',
+        method: function (state) {
+          main.loading = true;
+          reps(main.congressType, 'state', state).then(function (data) {
+            main.reps = data;
+            main.loading = false;
+          });
+        }
+      }
+    ];
 
-	main.searchSensByName = function(zip) {
-		reps.sensByName(name).then(function (data) {
-			main.reps = data;
-		});
-	};
-
-	main.searchSensByState = function(zip) {
-		reps.sensByState(state).then(function (data) {
-			main.reps = data;
-		});
-	};
-});
+    main.criteria = main.apis[0];
+  });
 
 angular
-	.module('repsService', [])
-	.factory('reps', function($http) {
-		var host = 'http://dgm-representatives.herokuapp.com';
+  .module('repsService', [])
+  .factory('reps', function ($http) {
+    var host = 'http://dgm-representatives.herokuapp.com';
 
-		function search(type, criteria, query) {
-			return $http
-			.get(host + '/' + type + '/by-' + criteria + '/' + query)
-			.then(function (response){
-				return response.data;
-			});
-		}
+    /**
+     * @function search
+     * @param {String} type - can be "all", "reps", "sens"
+     * @param {String} criteria - can by "zip", "name", "state"
+     * @param {String} query - can any string
+     */
+    function search(type, criteria, query) {
+      return $http
+        .get(host + '/' + type + '/by-' + criteria + '/' + query)
+        .then(function (response) {
+          return response.data;
+        });
+    }
 
-		search.allByZip = search.bind(null, 'all', 'zip');
-		search.repsByName = search.bind(null, 'reps', 'name');
-		search.repsByState = search.bind(null, 'reps', 'state');
-		search.sensByName = search.bind(null, 'sens', 'name');
-		search.sensByState = search.bind(null, 'sens', 'state');
-
-		return search
-});
+    return search;
+  });
